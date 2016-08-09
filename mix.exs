479e0239c046503399a9dbddc6d869509ba698b8
techgaun <coolsamar207@gmail.com>
2016-08-08 22:44:34 -0500

    add appropriate packages

--- mix.exs
+++ mix.exs
@@ -4,9 +4,17 @@ defmodule Solextria.Mixfile do
   def project do
     [app: :solextria,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     description: "Solectria DataLogger data fetcher and parser",
+     source_url: "https://github.com/Brightergy/solextria",
+     homepage_url: "https://github.com/Brightergy/solextria",
+     package: package,
+     elixir: "~> 1.2 or ~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test, "coveralls.semaphore": :test],
+     docs: [logo: "logo/brighterlink_logo.png",
+            extras: ["README.md"]],
      deps: deps()]
   end
 
@@ -27,6 +35,22 @@ defmodule Solextria.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:httpoison, "~> 0.9.0"},
+      {:floki, "~> 0.10.0"},
+      {:ex_doc, "~> 0.12", only: :dev},
+      {:excoveralls, "~> 0.5", only: :test},
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: [
+        "Samar Acharya"
+      ],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/Brightergy/egaugex"},
+      files: ~w(config lib logo mix.exs README.md LICENSE)
+    ]
   end
 end
