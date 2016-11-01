3f3921096ae92017e223a6347dc1b8055c25c1fa
techgaun <coolsamar207@gmail.com>
2016-11-01 02:56:32 -0500

    update doc

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Solextria.Mixfile do
 
   def project do
     [app: :solextria,
-     version: "0.1.1",
+     version: "0.1.2",
      description: "Solectria DataLogger data fetcher and parser",
      source_url: "https://github.com/Brightergy/solextria",
      homepage_url: "https://github.com/Brightergy/solextria",
